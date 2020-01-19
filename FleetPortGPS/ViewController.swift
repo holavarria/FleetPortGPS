6361b4f54dcbf6f3c8e704f5302138816fc4c2a6
Hernan Olavarria Venegas <holavarria@gmail.com>
2020-01-19 15:02:24 -0300

    Initial Commit

---
+++ FleetPortGPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FleetPortGPS
+//
+//  Created by Hernan Olavarria on 19/1/20.
+//  Copyright © 2020 FleetPort Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FleetPortGPSTests/FleetPortGPSTests.swift
@@ -0,0 +1,34 @@
+//
+//  FleetPortGPSTests.swift
+//  FleetPortGPSTests
+//
+//  Created by Hernan Olavarria on 19/1/20.
+//  Copyright © 2020 FleetPort Inc. All rights reserved.
+//
+
+import XCTest
+@testable import FleetPortGPS
+
+class FleetPortGPSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
